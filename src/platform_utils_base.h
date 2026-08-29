2abc8f54dd502ec9439395abebc69ab6c821ff78
siar1234 <siar019283@gmail.com>
2026-08-29 14:45:14 +0900

    feat: add pure virtual method to get device name

--- src/platform_utils_base.h
+++ src/platform_utils_base.h
@@ -7,6 +7,7 @@ class IPlatformUtilsBase
 public:
     virtual ~IPlatformUtilsBase() = default;
     [[nodiscard]] virtual std::string get_file_sha256_checksum(const std::string& file_path) const = 0;
+    [[nodiscard]] virtual std::string get_device_name() const = 0;
 };
 
 #endif //SHARED_PLATFORM_UTILS_H
