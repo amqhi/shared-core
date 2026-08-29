8876ab0c440a959675af83086d1dc3d0e362444a
siar1234 <siar019283@gmail.com>
2026-08-29 14:36:35 +0900

    feat: add RAII wrapper for SQLite statement

--- src/utils/sqlite_utils.h
+++ src/utils/sqlite_utils.h
@@ -10,4 +10,47 @@ namespace sqlite_utils
     std::optional<std::string> get_optional_string(sqlite3_stmt* stmt, int column);
 }
 
+struct Sqlite3Stmt
+{
+    sqlite3_stmt* stmt = nullptr;
+
+    Sqlite3Stmt() = default;
+    explicit Sqlite3Stmt(sqlite3_stmt* s) : stmt(s) {}
+
+    int bind(int index, const void* ptr, int size) const
+    {
+        return bind(index, ptr, size, SQLITE_TRANSIENT);
+    }
+
+    int bind(int index, const void* ptr, int size, sqlite3_destructor_type destructor) const
+    {
+        return sqlite3_bind_blob(stmt, index, ptr, size, destructor);
+    }
+
+    int prepare(
+    sqlite3 *db,            /* Database handle */
+const char *zSql,       /* SQL statement, UTF-8 encoded */
+int nByte,              /* Maximum length of zSql in bytes. */
+const char **pzTail     /* OUT: Pointer to unused portion of zSql */
+)
+    {
+        return sqlite3_prepare_v2(db, zSql, nByte, &stmt, nullptr);
+    }
+
+    int prepare(sqlite3 *db, const char *zSql)
+    {
+        return prepare(db, zSql, -1, nullptr);
+    }
+
+    [[nodiscard]] int step() const
+    {
+        return sqlite3_step(stmt);
+    }
+
+    ~Sqlite3Stmt()
+    {
+        sqlite3_finalize(stmt);
+    }
+};
+
 #endif //SHARED_SQLITE_UTILS_H
